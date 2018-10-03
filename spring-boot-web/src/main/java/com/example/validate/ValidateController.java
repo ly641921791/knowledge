8a1f8fdd3f34157cae1513a2ea7246677c9cc7ed
ly <641921791@qq.com>
2018-10-03 17:00:44 +0800

    提交校验框架总结

--- spring-boot-web/src/main/java/com/example/validate/ValidateController.java
+++ spring-boot-web/src/main/java/com/example/validate/ValidateController.java
@@ -1,10 +1,114 @@
 package com.example.validate;
 
+import lombok.Data;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 @RestController
+@RequestMapping("/validate")
 public class ValidateController {
 
+	@RequestMapping("/addUser")
+	public Integer addUser(@Validated(User.Add.class) User user) {
+		// 假设插入数据库后，生成id为89757
+		user.setId(89757);
+		// 插入成功后返回id
+		return user.getId();
+	}
 
+	@RequestMapping("/updateUser")
+	public boolean updateUser(@Validated(User.Update.class) User user) {
+		// 假设修改成功
+		return true;
+	}
 
+	@RequestMapping("/handlerBindingResult")
+	public Object bindingResult(@Validated(User.Add.class) User user, BindingResult bindingResult) {
+		// 若存在校验异常则处理
+		if (bindingResult.hasErrors()) {
+			StringBuffer errorMessage = new StringBuffer();
+			// 遍历全部校验异常并拼接异常信息
+			for (FieldError error : bindingResult.getFieldErrors()) {
+				errorMessage.append(error.getField()).append(error.getDefaultMessage()).append('\n');
+			}
+			return errorMessage.toString();
+		}
+		return true;
+	}
+}
+
+@Data
+class User {
+	// 定义新增校验组类
+	public interface Add {}
+
+	// 定义更新校验组类
+	public interface Update {}
+
+	// 添加校验并指定不同的校验组
+	@Null(groups = Add.class)
+	@NotNull(groups = Update.class)
+	private Integer id;
+
+	// 添加新增和修改校验组
+	@NotBlank(groups = {Add.class, Update.class})
+	private String name;
+
+	@Sex(allowed = {0, 1, 2}, groups = {Add.class, Update.class})
+	private Integer sex;
+}
+
+@Documented
+@Retention(RUNTIME)
+@Constraint(validatedBy = SexValidator.class)
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@interface Sex {
+	String message() default "参数不合法";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	int[] allowed() default {1, 2};
+}
+
+class SexValidator implements ConstraintValidator<Sex, Integer> {
+
+	private Set<Integer> allowed = new HashSet<>(2);
+
+	@Override
+	public void initialize(Sex sex) {
+		for (int i : sex.allowed()) {
+			allowed.add(i);
+		}
+	}
+
+	@Override
+	public boolean isValid(Integer value, ConstraintValidatorContext context) {
+		return allowed.contains(value);
+	}
 }
