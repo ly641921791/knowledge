e39f7092236015648c708ad3473b6fc9844ad235
Ly641921791 <641921791@qq.com>
2018-10-17 18:09:30 +0800

    提交字符替换过滤器

--- spring-boot-mvc-validate/src/main/java/example/WebApplication.java
+++ spring-boot-mvc-validate/src/main/java/example/WebApplication.java
@@ -3,8 +3,12 @@ package example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ly
+ */
 @SpringBootApplication
 public class WebApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
