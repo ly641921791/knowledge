4cc61f7589baa3c97186d2ab2fe6108e923feeb5
Ly641921791 <641921791@qq.com>
2018-12-12 17:59:07 +0800

    提交AbstractBeanFactory

--- spring-security/src/main/java/com/example/security/SecurityApplication.java
+++ spring-security/src/main/java/com/example/security/SecurityApplication.java
@@ -3,14 +3,27 @@ package com.example.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 /**
  * @author ly
  */
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class,args);
-	}
+    public static void main(String[] args) {
+
+        //SpringApplication.run(SecurityApplication.class,args);
+
+        System.out.println(System.nanoTime());
+        System.out.println(System.nanoTime());
+
+        long id = 27144295230255L;
+        long ii = 2714429523025511111L;
+
+        long id1 = 1127144295230255111L;
+
+    }
 
 }
